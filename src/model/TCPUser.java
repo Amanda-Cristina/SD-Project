154a403cf240f55898acf5be7e88fc144a8dc9d3
GilsonJRS <gilsonjuniorcva@gmail.com>
2022-05-01 22:25:41 -0300

    updates

--- src/model/TCPUser.java
+++ src/model/TCPUser.java
@@ -1,12 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  *
@@ -21,8 +21,18 @@ public class TCPUser {
         
     }
     
-    public void conect(String ip, int port){
+    public void conect(String ip, int port) throws IOException{
         this.userSocket = new Socket(ip, port);
-        output = 
+        output = new PrintWriter(this.userSocket.getOutputStream(), true);
+        input = new BufferedReader(new InputStreamReader(this.userSocket.getInputStream()));
+    }
+    
+    public JSONObject sendMessage(JSONObject msg_json) throws IOException, JSONException{
+        this.output.print(msg_json.toString());
+        this.output.flush();
+        char[] cbuf = new char[2048];
+        input.read(cbuf);
+        JSONObject reply = new JSONObject(String.valueOf(cbuf));
+        return reply;
     }
 }
